25902db1880c663b65431d3fc9e8285398a434a2
Nerijus Šurkys <surkeela@gmail.com>
2023-11-26 20:05:29 +0200

    wip

--- CodeAcademyPay/Networking/NetworkManager.swift
+++ CodeAcademyPay/Networking/NetworkManager.swift
@@ -11,58 +11,60 @@ enum NetworkError: Error {
     case invalidURL
     case noData
     case networkError(Error)
+    case decodingError(Error)
+    case authenticationError
 }
 
 class NetworkManager {
-    private var authToken: String?
-
-    func createRequest(urlString: String, method: String, headers: [String: String]?, body: Data?) -> URLRequest? {
-        guard let url = URL(string: urlString) else { return nil }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = method
-
-        if let token = authToken {
-            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
-        }
-
-        if let headers = headers {
-            for (key, value) in headers {
-                request.setValue(value, forHTTPHeaderField: key)
-            }
-        }
-
-        request.httpBody = body
-
-        return request
-    }
-
-    func addBasicAuthHeader(request: inout URLRequest, phoneNumber: String, password: String) {
-        let loginString = "\(phoneNumber):\(password)"
-        guard let loginData = loginString.data(using: .utf8) else {
-            return
-        }
-
-        let base64LoginString = loginData.base64EncodedString()
-        let authString = "Basic \(base64LoginString)"
-        request.setValue(authString, forHTTPHeaderField: "Authorization")
-    }
-
-    func performRequest(with request: URLRequest, completion: @escaping (Result<Data, NetworkError>) -> Void) {
+    func performRequest<T: Decodable>(with request: URLRequest, completion: @escaping (Result<T, NetworkError>) -> Void) {
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
                 completion(.failure(.networkError(error)))
                 return
             }
-
+            
             guard let data = data else {
                 completion(.failure(.noData))
                 return
             }
-
-            completion(.success(data))
+            
+            do {
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodedData))
+            } catch {
+                completion(.failure(.decodingError(error)))
+            }
         }.resume()
     }
-
+    
+    func createRequest(urlString: String, method: String, headers: [String: String]?, body: Data?) throws -> URLRequest {
+        guard let url = URL(string: urlString) else { throw NetworkError.invalidURL }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method
+        
+        if let headers = headers {
+            for (key, value) in headers {
+                request.setValue(value, forHTTPHeaderField: key)
+            }
+        }
+        
+        request.httpBody = body
+        
+        return request
+    }
+    
+    func addBasicAuthHeader(request: inout URLRequest, phoneNumber: String, password: String) {
+        let loginString = "\(phoneNumber):\(password)"
+        guard let loginData = loginString.data(using: .utf8) else {
+            return
+        }
+        
+        let base64LoginString = loginData.base64EncodedString()
+        let authString = "Basic \(base64LoginString)"
+        request.setValue(authString, forHTTPHeaderField: "Authorization")
+    }
+    
 }
+
 
--- CodeAcademyPay/User/UserManagementViewModel.swift
+++ CodeAcademyPay/User/UserManagementViewModel.swift
@@ -10,76 +10,19 @@ import Foundation
 class UserManagementViewModel {
     private let networkManager = NetworkManager()
 
-    private func performRequest<T: Decodable>(urlString: String,
-                                             method: String,
-                                             headers: [String: String]?,
-                                             body: Data?,
-                                             completion: @escaping (Result<T, Error>) -> Void) {
-
-        guard let request = networkManager.createRequest(urlString: urlString, method: method, headers: headers, body: body) else {
-            completion(.failure(NetworkError.invalidURL))
-            return
-        }
-
-        networkManager.performRequest(with: request) { result in
-            switch result {
-            case .success(let data):
-                do {
-                    let decoder = JSONDecoder()
-                    let decodedData = try decoder.decode(T.self, from: data)
-                    completion(.success(decodedData))
-                } catch {
-                    completion(.failure(error))
-                }
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-
     func registerUser(userData: UserRegistrationData, completion: @escaping (Result<User, Error>) -> Void) {
         let urlString = Endpoints.register()
+        let headers = ["Content-Type": "application/json"]
+        
+        let networkManager = NetworkManager()
         do {
-            let encoder = JSONEncoder()
-            let requestBody = try encoder.encode(userData)
-            performRequest(urlString: urlString, method: "POST", headers: ["Content-Type": "application/json"], body: requestBody, completion: completion)
-        } catch {
-            completion(.failure(error))
-        }
-    }
-
-    func getAllUsers(completion: @escaping (Result<[User], Error>) -> Void) {
-        let urlString = Endpoints.allUsers()
-        performRequest(urlString: urlString, method: "GET", headers: nil, body: nil, completion: completion)
-    }
-
-    private func createLoginRequest(phoneNumber: String, password: String, urlString: String) throws -> URLRequest {
-        let userData = UserLoginData(phoneNumber: phoneNumber, password: password)
-
-        let encoder = JSONEncoder()
-        let requestBody = try encoder.encode(userData)
-
-        var request = URLRequest(url: URL(string: urlString)!)
-        request.httpMethod = "POST"
-        request.httpBody = requestBody
-
-        networkManager.addBasicAuthHeader(request: &request, phoneNumber: phoneNumber, password: password)
-
-        return request
-    }
-
-    func loginUser(phoneNumber: String, password: String, id: String, completion: @escaping (Result<UserLoginResponse, Error>) -> Void) {
-        let urlString = Endpoints.login()
-
-        do {
-            let request = try createLoginRequest(phoneNumber: phoneNumber, password: password, urlString: urlString)
-
-            networkManager.performRequest(with: request) { (result: Result<Data, NetworkError>) in
+            let requestBody = try JSONEncoder().encode(userData)
+            let request = try networkManager.createRequest(urlString: urlString, method: "POST", headers: headers, body: requestBody)
+            
+            networkManager.performRequest(with: request) { (result: Result<User, NetworkError>) in
                 switch result {
-                case .success(let data):
-                    let decoder = JSONDecoder()
-                    let decodingResult: Result<UserLoginResponse, Error> = Result { try decoder.decode(UserLoginResponse.self, from: data) }
-                    self.handleDecodingResult(decodingResult, completion: completion)
+                case .success(let user):
+                    completion(.success(user))
                 case .failure(let error):
                     completion(.failure(error))
                 }
@@ -89,19 +32,84 @@ class UserManagementViewModel {
         }
     }
 
+    func getAllUsers(completion: @escaping (Result<[User], Error>) -> Void) {
+        let urlString = Endpoints.allUsers()
+        
+        let networkManager = NetworkManager()
+        do {
+            let request = try networkManager.createRequest(urlString: urlString, method: "GET", headers: nil, body: nil)
+            
+            networkManager.performRequest(with: request) { (result: Result<[User], NetworkError>) in
+                switch result {
+                case .success(let users):
+                    completion(.success(users))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        } catch {
+            completion(.failure(error))
+        }
+    }
+
+    func createLoginRequest(phoneNumber: String, password: String, urlString: String) throws -> URLRequest {
+        guard let url = URL(string: urlString) else {
+            throw NetworkError.invalidURL
+        }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        // Add Basic Authentication Header
+        let loginString = "\(phoneNumber):\(password)"
+        guard let loginData = loginString.data(using: .utf8) else {
+            throw NetworkError.authenticationError
+        }
+        
+        let base64LoginString = loginData.base64EncodedString()
+        let authString = "Basic \(base64LoginString)"
+        request.setValue(authString, forHTTPHeaderField: "Authorization")
+
+        return request
+    }
+
+
+    func loginUser(phoneNumber: String, password: String, id: String, completion: @escaping (Result<UserLoginResponse, Error>) -> Void) {
+        let urlString = Endpoints.login()
+
+        do {
+            let request = try createLoginRequest(phoneNumber: phoneNumber, password: password, urlString: urlString)
+
+            networkManager.performRequest(with: request) { (result: Result<UserLoginResponse, NetworkError>) in
+                switch result {
+                case .success(let userLoginResponse):
+                    completion(.success(userLoginResponse))
+                case .failure(let networkError):
+                    completion(.failure(networkError))
+                }
+            }
+        } catch {
+            completion(.failure(error))
+        }
+    }
 
     func fetchDataWithBearerToken(userID: String, bearerToken: String, completion: @escaping (Result<AuthenticatedUser, Error>) -> Void) {
         let urlString = Endpoints.user(withID: userID)
         let headers = ["Authorization": "Bearer \(bearerToken)"]
 
-        performRequest(urlString: urlString, method: "GET", headers: headers, body: nil, completion: completion)
-    }
-    
-    private func handleDecodingResult<T: Decodable>(_ decodingResult: Result<T, Error>, completion: @escaping (Result<T, Error>) -> Void) {
-        switch decodingResult {
-        case .success(let decodedData):
-            completion(.success(decodedData))
-        case .failure(let error):
+        let networkManager = NetworkManager()
+        do {
+            let request = try networkManager.createRequest(urlString: urlString, method: "GET", headers: headers, body: nil)
+
+            networkManager.performRequest(with: request) { (result: Result<AuthenticatedUser, NetworkError>) in
+                switch result {
+                case .success(let authenticatedUser):
+                    completion(.success(authenticatedUser))
+                case .failure(let networkError):
+                    completion(.failure(networkError))
+                }
+            }
+        } catch {
             completion(.failure(error))
         }
     }
