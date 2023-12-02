438d3d915e1214f8c21ea666eab5cdacddee851a
Nerijus Šurkys <surkeela@gmail.com>
2023-12-02 16:01:08 +0200

    created TransactionsListViewController

---
+++ CodeAcademyPay/Scenes/TransactionsListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TransactionsListViewController.swift
+//  CodeAcademyPay
+//
+//  Created by Nerijus Surkys on 2023-12-02.
+//
+
+import UIKit
+
+class TransactionsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
